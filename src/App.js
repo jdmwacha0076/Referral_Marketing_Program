cdb69da9e565fd74e0d7e97186f6db8259591eb5
JosephMwacha <joseph.mwacha@ucglobalprograms.org>
2023-12-24 09:40:40 +0300

    Created the register and login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/registration/Registration';
+import Login from './components/login/Login';
+import Home from './components/shared/home';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
